cb30e32932303b12be30962bb33ebc1e0899cd79
kminny <darkdevil94@naver.com>
2021-04-02 16:30:33 +0900

    Room Detail API

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,16 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import BigRoomSerializer, RoomSerializer
 
 
 class ListRoomsView(ListAPIView):
 
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class SeeRoomView(RetrieveAPIView):
+
+    queryset = Room.objects.all()
+    serializer_class = BigRoomSerializer
