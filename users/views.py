d813f8eb644e6786de8411ee1d03bd752add0cdb
kminny <darkdevil94@naver.com>
2021-04-02 20:02:24 +0900

    FavViews & Room Serializer modified

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rooms.models import Room
+from rooms.serializers import RoomSerializer
 from .models import User
 from .serializers import ReadUserSerializer, WriteUserSerializer
 
@@ -32,3 +34,28 @@ def user_detail(request, pk):
         return Response(ReadUserSerializer(user).data, status=status.HTTP_200_OK)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FavsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = RoomSerializer(user.favs.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        pk = request.data.get("pk", None)
+        user = request.user
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.remove(room)
+                else:
+                    user.favs.add(room)
+                return Response(status=status.HTTP_200_OK)
+            except Room.DoesNotExist:
+                pass
+        return Response(status=status.HTTP_400_BAD_REQUEST)
