6d3c919eac895a67bdf4435f67fecbc4b3b1698d
kminny <darkdevil94@naver.com>
2021-04-03 18:22:31 +0900

    Searching 02 by coords

--- rooms/views.py
+++ rooms/views.py
@@ -79,6 +79,9 @@ def room_search(request):
     beds = request.GET.get("beds", None)
     bedrooms = request.GET.get("bedrooms", None)
     bathrooms = request.GET.get("bathrooms", None)
+    lat = request.GET.get("lat", None)
+    lng = request.GET.get("lng", None)
+
     filter_kwargs = {}
     if max_price is not None:
         filter_kwargs["price__lte"] = max_price
@@ -90,11 +93,16 @@ def room_search(request):
         filter_kwargs["bedrooms__gte"] = bedrooms
     if bathrooms is not None:
         filter_kwargs["bathrooms__gte"] = bathrooms
-    paginator = OwnPagination()
+    if lat is not None and lng is not None:
+        filter_kwargs["lat__gte"] = float(lat) - 0.005
+        filter_kwargs["lat__lte"] = float(lat) + 0.005
+        filter_kwargs["lng__gte"] = float(lng) - 0.005
+        filter_kwargs["lng__lte"] = float(lng) + 0.005
     try:
         rooms = Room.objects.filter(**filter_kwargs)
     except ValueError:
         rooms = Room.objects.all()
+    paginator = OwnPagination()
     results = paginator.paginate_queryset(rooms, request)
     serializer = RoomSerializer(results, many=True)
 
