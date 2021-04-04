12a7fa3e0eb669bfdb6858a3111cb4524776eb6a
kminny <darkdevil94@naver.com>
2021-04-04 12:18:31 +0900

    Creating Room with user

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,5 @@
-from rest_framework import permissions, status
+from rest_framework import permissions
 from rest_framework.decorators import api_view
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Room
 from .permissions import IsOwner
